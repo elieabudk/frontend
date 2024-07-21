bc28d085abbe59b14fa8c0404043fdff855a2252
elieabudk <elieabud@hotmail.com>
2024-07-20 19:08:48 -0500

    practica div

---
+++ segundo_sitio_web/script.js
@@ -0,0 +1,22 @@
+console.log("hola")
+
+function myFun() {
+    document.getElementById("titulo_main")
+        .innerHTML = "Contenido cambiado!";
+}
+
+
+let a, b, c;
+a = 2;
+b = 3;
+c = a + b;
+console.log("The value of c is " + c + ".");
+
+function myFunction() {
+    console.log("Hello");
+    console.log("How are you?");
+}
+
+myFunction()
+
+
